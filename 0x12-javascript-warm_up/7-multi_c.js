152bec290827bca44a9f50cdc85de2587d9817bc
AlvinOt <thisisalvinhere@gmail.com>
2023-04-11 23:22:38 +0300

    use let

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const multi_c = process.argv[2];
 if (!parseInt(multi_c)) {
   console.log('Missing number of occurrences');
 } else {
-  for (i = 0; i < multi_c; i++) {
+  for (let i = 0; i < multi_c; i++) {
     console.log('C is fun');
   }
 }
