5b19215b877e90720f122b188c0d4d9db42f1208
AlvinOt <thisisalvinhere@gmail.com>
2023-04-12 22:19:44 +0300

    t0

---
+++ 0x13-javascript_objects_scopes_closures/forswitch.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 5; i++) {
+  switch (i) {
+    case 0:
+      console.log("i is 0");
+      break;
+    case 1:
+      console.log("i is 1");
+      break;
+    case 2:
+      console.log("i is 2");
+      break;
+    default:
+      console.log("i is greater than 2");
+  }
+}
+
