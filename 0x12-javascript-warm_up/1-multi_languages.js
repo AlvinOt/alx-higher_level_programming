af1fc4f7aa66d2591d598e48e2e6595b0ce26c0a
AlvinOt <thisisalvinhere@gmail.com>
2023-04-12 20:23:02 +0300

    another1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+//A script that prints 3 lines
 
 const myVar1 = 'C is fun';
 const myVar2 = 'Python is cool';
-const myVar3 = 'Javascript is amazing';
+const myVar3 = 'JavaScript is amazing';
 
 console.log(myVar1 + '\n' + myVar2 + '\n' + myVar3);
 
